ea3d250bf751d2ae37121c0929be354575be4ed5
DarkLBP <darklbp@outlook.com>
2017-05-22 12:26:02 +0200

    Release 1.1.0

--- DESINFECTAR ORDENADOR.bat
+++ DESINFECTAR ORDENADOR.bat
@@ -1,14 +1,18 @@
 @echo off
-title Herramienta de eliminacion de Forbix (Escritorio)
-echo FORBIX REMOVAL TOOL
-echo Version 1.0.0
+color a
+title Herramienta de eliminacion de Forbix (Ordenador)
+echo -----------------------
+echo --FORBIX REMOVAL TOOL--
+echo ----Version 1.1.0------
+echo -----------------------
 echo Creado por Leandro Botella
 echo.
-echo Esta herramienta eliminara la infeccion de USB Forbix de su ordenador
-echo Se recomienda guardar su trabajo antes de continuar
+echo Esta herramienta eliminara la infeccion de USB Forbix de su ordenador.
+echo Se recomienda guardar su trabajo antes de continuar.
 echo Pulse cualquier tecla para empezar el proceso...
 pause>nul
 cls
+color e
 echo Cerrando proceso wscript.exe.
 taskkill /im wscript.exe -f
 echo Aplicando cambio de atributos.
@@ -17,6 +21,8 @@ echo Eliminando archivo infectado.
 del /F /Q %tmp%\SysinfY2X.db 
 echo Aplicando cambios al registro.
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Shared Tools\MSConfig\startupreg\SysinfY2X" /f
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v SysinfY2X /f
+color b
 echo.
 echo Proceso terminado! Si algun paso anterior genero un error es posible que la infeccion persista.
 echo Creado por Leandro Botella.
--- DESINFECTAR USB.bat
+++ DESINFECTAR USB.bat
@@ -1,28 +1,35 @@
 @echo off
 title Herramienta de eliminacion de Forbix (USB)
-echo FORBIX REMOVAL TOOL
-echo Version 1.0.0
+color a
+echo -----------------------
+echo --FORBIX REMOVAL TOOL--
+echo ----Version 1.1.0------
+echo -----------------------
 echo Creado por Leandro Botella
 echo.
-echo Esta herramienta eliminara la infeccion de USB Forbix de su USB
-echo Se recomienda guardar su trabajo antes de continuar
+echo Esta herramienta eliminara la infeccion de USB Forbix de su USB.
+echo Se recomienda guardar su trabajo antes de continuar.
 echo Pulse cualquier tecla para empezar el proceso...
 pause>nul
 cls
-set /p DriveLetter=Introduzca la letra de la unidad (Ejemplo F:).
+set /p DriveLetter=Introduzca la letra de la unidad (Ejemplo F:): 
 %DriveLetter%
 cd \
+cls
+color c
 echo Has seleccionado %DriveLetter%. Asegurate que la letra es correcta y que que tenga el caracter : despues de ella.
 echo Si no se especifica correctamente podria causar problemas en el sistema.
 echo Pulsa cualquier tecla despues de comprobar la letra.
 pause>nul
 cls
+color e
 echo Aplicando cambio de atributos.
 attrib -h -r -s /s /d *.*
 echo Eliminando archivo infectado.
 del /F /Q Manuel.doc
 echo Eliminando accesos directos.
 del /F /Q *.lnk
+color b
 echo.
 echo Proceso terminado! Si algun paso anterior genero un error es posible que la infeccion persista.
 echo Creado por Leandro Botella.
